a0a2dff8c65727a50885145e692f37e7c8341c4b
test <283507383@qq.com>
2018-03-27 10:23:31 +0800

    修改SampleMovie

--- neo4j/src/main/java/movies/spring/data/neo4j/SampleMovieApplication.java
+++ neo4j/src/main/java/movies/spring/data/neo4j/SampleMovieApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
- * @author Michael Hunger
+ * @author Xiang fusheng
  * @author Mark Angrish
  */
 @SpringBootApplication
